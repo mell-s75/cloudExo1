d4539ae38f8cc9fdb9b70426b69f14a3b6a90ba7
Mell <mell1999@hotmail.fr>
2021-01-21 16:11:17 +0100

    database

--- config/database.js
+++ config/database.js
@@ -1,15 +1,19 @@
 module.exports = ({ env }) => ({
   defaultConnection: 'default',
   connections: {
-    default: {
+      default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+      client: 'postgres',
+      host: 'ec2-54-73-253-140.eu-west-1.compute.amazonaws.com',
+      port: '5432',
+      database: 'dfel3un8k62r6l',
+      username: 'gftnynttiqojbr',
+      password: 'a9005149761a6da8a7b98f65ee7121fd9e7fde4510644a50c0def80a774032d0',
       },
       options: {
-        useNullAsDefault: true,
+      ssl: false,
       },
-    },
-  },
-});
+      },
+      },
+      });
